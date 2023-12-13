b798570adcdf9e683c8c93114472045c005ce219
Nikita Kozyr <n1kk0.ipd2@gmail.com>
2023-12-13 17:18:42 +0300

    little fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/app';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {storeSetup} from './services/store';
 
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={storeSetup()}>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
